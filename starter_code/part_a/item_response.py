d38efc20c5c2fa1c60c09895d0988a4d5cd5b6f1
Runshi Yang <runshi.yang@mail.utoronto.ca>
2022-11-25 02:18:36 -0500

    finished Part A Q1 and Q2

--- starter_code/part_a/item_response.py
+++ starter_code/part_a/item_response.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 from utils import *
 
 import numpy as np
@@ -24,7 +26,17 @@ def neg_log_likelihood(data, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    log_lklihood = 0.
+    log_lklihood = 0
+    user_id = data["user_id"]
+    question_id = data["question_id"]
+    is_correct = data["is_correct"]
+
+    for k in range(len(user_id)):
+        theta_k = theta[user_id[k]]
+        beta_k = beta[question_id[k]]
+        c_k = is_correct[k]
+        log_lklihood += c_k * (theta_k - beta_k) - np.logaddexp(0, theta_k - beta_k)
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -52,7 +64,16 @@ def update_theta_beta(data, lr, theta, beta):
     # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    pass
+    user_id = data["user_id"]
+    question_id = data["question_id"]
+    is_correct = data["is_correct"]
+
+    for k in range(len(user_id)):
+        theta_k = theta[user_id[k]]
+        beta_k = beta[question_id[k]]
+        c_k = is_correct[k]
+        theta[user_id[k]] += lr * (c_k - sigmoid(theta_k - beta_k))
+        beta[question_id[k]] += lr * (sigmoid(theta_k - beta_k) - c_k)
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -73,20 +94,25 @@ def irt(data, val_data, lr, iterations):
     :return: (theta, beta, val_acc_lst)
     """
     # TODO: Initialize theta and beta.
-    theta = None
-    beta = None
+    theta = np.zeros(len(data["user_id"]) + 1)
+    beta = np.zeros(len(data["question_id"]) + 1)
 
     val_acc_lst = []
+    train_lld_lst = []
+    val_lld_lst = []
 
     for i in range(iterations):
-        neg_lld = neg_log_likelihood(data, theta=theta, beta=beta)
+        neg_lld_train = neg_log_likelihood(data, theta=theta, beta=beta)
+        neg_lld_val = neg_log_likelihood(val_data, theta=theta, beta=beta)
+        train_lld_lst.append(-neg_lld_train)
+        val_lld_lst.append(-neg_lld_val)
         score = evaluate(data=val_data, theta=theta, beta=beta)
         val_acc_lst.append(score)
-        print("NLLK: {} \t Score: {}".format(neg_lld, score))
+        print("NLLK: {} \t Score: {}".format(neg_lld_train, score))
         theta, beta = update_theta_beta(data, lr, theta, beta)
 
     # TODO: You may change the return values to achieve what you want.
-    return theta, beta, val_acc_lst
+    return theta, beta, val_acc_lst, train_lld_lst, val_lld_lst
 
 
 def evaluate(data, theta, beta):
@@ -111,7 +137,7 @@ def evaluate(data, theta, beta):
 def main():
     train_data = load_train_csv("../data")
     # You may optionally use the sparse matrix.
-    sparse_matrix = load_train_sparse("../data")
+    # sparse_matrix = load_train_sparse("../data")
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
@@ -120,7 +146,31 @@ def main():
     # Tune learning rate and number of iterations. With the implemented #
     # code, report the validation and test accuracy.                    #
     #####################################################################
-    pass
+    # part (b)
+    theta1, beta1, val_acc_lst1, train_lld_lst1, val_lld_lst1 = irt(train_data, val_data, 0.01, 50)
+    theta2, beta2, val_acc_lst2, train_lld_lst2, val_lld_lst2 = irt(train_data, val_data, 0.02, 25)
+    theta3, beta3, val_acc_lst3, train_lld_lst3, val_lld_lst3 = irt(train_data, val_data, 0.05, 10)
+    theta4, beta4, val_acc_lst4, train_lld_lst4, val_lld_lst4 = irt(train_data, val_data, 0.001, 80)
+
+    print("validation rate accuracy 1:", val_acc_lst1)
+    print("validation rate accuracy 2:", val_acc_lst2)
+    print("validation rate accuracy 3:", val_acc_lst3)
+    print("validation rate accuracy 4:", val_acc_lst4)
+
+    plt.figure()
+    plt.plot(train_lld_lst4, label="training")
+    plt.plot(val_lld_lst4, label="validation")
+    plt.xlabel("number of iterations")
+    plt.ylabel("log-likelihood")
+    plt.title("Training and Validation Log-likelihoods as a Function of Iterations")
+    plt.legend()
+    plt.show()
+
+    # part (c)
+    val_acc = evaluate(val_data, theta4, beta4)
+    print("Validation Accuracy:", val_acc)
+    test_acc = evaluate(test_data, theta4, beta4)
+    print("Test Accuracy:", test_acc)
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -129,7 +179,19 @@ def main():
     # TODO:                                                             #
     # Implement part (d)                                                #
     #####################################################################
-    pass
+    j1 = sigmoid(np.sort(theta4) - beta4[0])
+    j2 = sigmoid(np.sort(theta4) - beta4[1])
+    j3 = sigmoid(np.sort(theta4) - beta4[2])
+
+    plt.figure()
+    plt.plot(np.sort(theta4), j1, label="Question 1")
+    plt.plot(np.sort(theta4), j2, label="Question 2")
+    plt.plot(np.sort(theta4), j3, label="Question 3")
+    plt.title("Probability of Correct Response as a function of Theta given a Question j")
+    plt.ylabel("probability of the correct response")
+    plt.xlabel("theta given a question j")
+    plt.legend()
+    plt.show()
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
--- starter_code/part_a/knn.py
+++ starter_code/part_a/knn.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from sklearn.impute import KNNImputer
 from utils import *
 
@@ -19,7 +20,8 @@ def knn_impute_by_user(matrix, valid_data, k):
     # We use NaN-Euclidean distance measure.
     mat = nbrs.fit_transform(matrix)
     acc = sparse_matrix_evaluate(valid_data, mat)
-    print("Validation Accuracy: {}".format(acc))
+    # TODO: double check if we can change the output format
+    print("Accuracy: {}".format(acc))
     return acc
 
 
@@ -33,14 +35,11 @@ def knn_impute_by_item(matrix, valid_data, k):
     :param k: int
     :return: float
     """
-    #####################################################################
-    # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
-    #####################################################################
-    acc = None
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    nbrs = KNNImputer(n_neighbors=k)
+    # We use NaN-Euclidean distance measure.
+    mat = nbrs.fit_transform(matrix.T)
+    acc = sparse_matrix_evaluate(valid_data, mat.T)
+    print("Accuracy: {}".format(acc))
     return acc
 
 
@@ -54,16 +53,50 @@ def main():
     print("Shape of sparse matrix:")
     print(sparse_matrix.shape)
 
-    #####################################################################
-    # TODO:                                                             #
-    # Compute the validation accuracy for each k. Then pick k* with     #
-    # the best performance and report the test accuracy with the        #
-    # chosen k*.                                                        #
-    #####################################################################
-    pass
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    k_list = [1, 6, 11, 16, 21, 26]
+
+    valid_acc_by_user = []
+    max_acc_user = 0
+    k_user = 0
+    print("kNN imputed by user:")
+    for k in k_list:
+        print(f"When k is {k}, Validation ", end='')
+        accuracy = knn_impute_by_user(sparse_matrix, val_data, k)
+        valid_acc_by_user.append(accuracy)
+        if accuracy > max_acc_user:
+            k_user = k
+            max_acc_user = accuracy
+    print(f"k* = {k_user} has the highest performance on validation data, with Test ", end='')
+    test_acc_user = knn_impute_by_user(sparse_matrix, test_data, k_user)
+    print("\n")
+
+    valid_acc_by_item = []
+    max_acc_item = 0
+    k_item = 0
+    print("kNN imputed by item:")
+    for k in k_list:
+        print(f"When k is {k}, Validation ", end='')
+        accuracy = knn_impute_by_item(sparse_matrix, val_data, k)
+        valid_acc_by_item.append(accuracy)
+        if accuracy > max_acc_item:
+            k_item = k
+            max_acc_item = accuracy
+    print(f"k* = {k_item} has the highest performance on validation data, with Test ", end='')
+    test_acc_item = knn_impute_by_item(sparse_matrix, test_data, k_item)
+
+    plt.figure(1)
+    plt.plot(k_list, valid_acc_by_user)
+    plt.xlabel("k")
+    plt.ylabel("validation accuracy")
+    plt.title("Validation Accuracy of kNN Imputed by User")
+    plt.show()
+
+    plt.figure(2)
+    plt.plot(k_list, valid_acc_by_item)
+    plt.xlabel("k")
+    plt.ylabel("validation accuracy")
+    plt.title("Validation Accuracy of kNN Imputed by Item")
+    plt.show()
 
 
 if __name__ == "__main__":
